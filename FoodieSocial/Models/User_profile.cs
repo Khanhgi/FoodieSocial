496b01b354630bbcea9b6e19f26cd0a5c64fe75e
Khánh Võ An <voankhanh2k2@gmail.com>
2023-06-03 08:45:06 +0700

    làm tiếp chức năng post và sửa chức năng friend

--- FoodieSocial/Models/User_profile.cs
+++ FoodieSocial/Models/User_profile.cs
@@ -19,26 +19,21 @@ namespace FoodieSocial.Models
 
         public int Id { get; set; }
 
-        [Required]
         [StringLength(100)]
         public string Email { get; set; }
 
-        [Required]
         [StringLength(50)]
         public string PassWord { get; set; }
 
-        [Required]
         [StringLength(255)]
         public string Name { get; set; }
 
         [StringLength(10)]
         public string Gender { get; set; }
 
-        [Required]
         [StringLength(50)]
         public string PhoneNumber { get; set; }
 
-        [Required]
         [StringLength(255)]
         public string Country { get; set; }
 
